79af8373f8000f3020876091e58cb3ebb08b8545
Arthur Kozhemyakin <trable228@icloud.com>
2025-09-17 17:25:21 +0300

    saved images fragment with mock info

--- app/src/main/java/com/android/photogallery/SavedImagesFragment.kt
+++ app/src/main/java/com/android/photogallery/SavedImagesFragment.kt
@@ -1,30 +1,81 @@
 package com.android.photogallery
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import com.android.photogallery.databinding.FragmentSavedImagesBinding
+import com.android.photogallery.models.ImageResult
 
 class SavedImagesFragment : Fragment() {
 
-    private lateinit var binding: FragmentSavedImagesBinding
+    private lateinit var binding: FragmentSavedImagesBinding;
 
-    companion object {
-        fun newInstance(): ImageDetailFragment {
-            val fragment = ImageDetailFragment()
-            return fragment
-        }
-    }
+    private lateinit var adapter: ImageAdapter
+    private val savedImages = mutableListOf<ImageResult>()
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: android.view.LayoutInflater,
+        container: android.view.ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
         binding = FragmentSavedImagesBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (savedImages.isEmpty()) {
+            savedImages.addAll(createTestImages())
+        }
+
+        setupRecyclerView()
+        updateEmptyState()
+    }
+
+    private fun createTestImages(): List<ImageResult> {
+        return listOf(
+            ImageResult(
+                id = "1",
+                title = "Test Image 1",
+                url = "https://karton33.ru/images/kristall/korob.png",
+                thumbnail = null,
+                creator = "Test Creator",
+                creator_url = null,
+                license = "CC0",
+                license_version = "1.0",
+                license_url = null,
+                provider = "Test",
+                source = "Test Source",
+                tags = null,
+                attribution = null,
+                height = 500,
+                width = 500
+            )
+        )
+    }
+
+    private fun setupRecyclerView() {
+        binding.savedImagesRecyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
+
+        adapter = ImageAdapter(savedImages) { image ->
+            showImageDetail(image)
+        }
+        binding.savedImagesRecyclerView.adapter = adapter
+    }
+
+    private fun showImageDetail(image: ImageResult) {
+        (requireActivity() as MainActivity).showImageDetail(image)
+    }
+
+    private fun updateEmptyState() {
+        if (savedImages.isEmpty()) {
+            binding.emptyStateText.visibility = View.VISIBLE
+            binding.savedImagesRecyclerView.visibility = View.GONE
+        } else {
+            binding.emptyStateText.visibility = View.GONE
+            binding.savedImagesRecyclerView.visibility = View.VISIBLE
+        }
+    }
 }
