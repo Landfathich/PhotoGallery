3b6c06d85bcaf08d26524cd1b63109e7a227941a
Arthur Kozhemyakin <trable228@icloud.com>
2026-01-16 11:13:03 +0300

    use activityViewModels for saving search results between fragments

--- app/src/main/java/com/android/photogallery/fragments/SearchFragment.kt
+++ app/src/main/java/com/android/photogallery/fragments/SearchFragment.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -22,7 +22,7 @@ class SearchFragment : Fragment() {
 
     private lateinit var binding: FragmentSearchBinding
     private lateinit var adapter: ImageAdapter
-    private val viewModel: SearchViewModel by viewModels()
+    private val viewModel: SearchViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
